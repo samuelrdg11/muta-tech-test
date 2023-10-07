53af72868efcd7bb42446044a83f64e51b1bd730
samuelrdg11 <samuelrdg11@gmail.com>
2023-10-07 01:07:10 -0500

    update app

--- App.js
+++ App.js
@@ -1,20 +1,139 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./components/HomeScreen";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+const styles = StyleSheet.create({
+  tabBar: {
+    position: "absolute",
+    paddingTop: 2,
+    bottom: 0,
+    height: 95,
+    backgroundColor: "#181818",
+    borderTopColor: "transparent",
+  },
+});
+
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="light" />
+      <BottomTab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: styles.tabBar,
+        }}
+      >
+        <BottomTab.Screen
+          component={HomeScreen}
+          name="Inicio"
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <>
+                  <Icon
+                    name="home"
+                    size={30}
+                    color={focused ? "#d9f264" : "#8c8c8c"}
+                  />
+                  <Text
+                    style={{
+                      fontSize: 11,
+                      color: focused ? "#d9f264" : "#8c8c8c",
+                      marginTop: 3,
+                    }}
+                  >
+                    Inicio
+                  </Text>
+                </>
+              );
+            },
+          }}
+        />
+        <BottomTab.Screen
+          component={HomeScreen}
+          name="Billetera"
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <>
+                  <Icon
+                    name="wallet"
+                    size={30}
+                    color={focused ? "#d9f264" : "#8c8c8c"}
+                  />
+                  <Text
+                    style={{
+                      fontSize: 11,
+                      color: focused ? "#d9f264" : "#8c8c8c",
+                      marginTop: 3,
+                    }}
+                  >
+                    Billetera
+                  </Text>
+                </>
+              );
+            },
+          }}
+        />
+        <BottomTab.Screen
+          component={HomeScreen}
+          name="Recolecciones"
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <>
+                  <Icon
+                    name="calendar"
+                    size={30}
+                    color={focused ? "#d9f264" : "#8c8c8c"}
+                  />
+                  <Text
+                    style={{
+                      fontSize: 11,
+                      color: focused ? "#d9f264" : "#8c8c8c",
+                      marginTop: 3,
+                    }}
+                  >
+                    Recolecciones
+                  </Text>
+                </>
+              );
+            },
+          }}
+        />
+        <BottomTab.Screen
+          component={HomeScreen}
+          name="Perfil"
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <>
+                  <Icon
+                    name="account-outline"
+                    size={30}
+                    color={focused ? "#d9f264" : "#8c8c8c"}
+                  />
+                  <Text
+                    style={{
+                      fontSize: 11,
+                      color: focused ? "#d9f264" : "#8c8c8c",
+                      marginTop: 3,
+                    }}
+                  >
+                    Perfil
+                  </Text>
+                </>
+              );
+            },
+          }}
+        />
+      </BottomTab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
