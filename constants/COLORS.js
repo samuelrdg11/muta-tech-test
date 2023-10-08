63cf04a0808338c15bd9869aa572813ea7ecbc69
samuelrdg11 <samuelrdg11@gmail.com>
2023-10-08 01:08:08 -0500

    change:: update constants files

---
+++ constants/COLORS.js
@@ -0,0 +1,9 @@
+export default COLORS = {
+  text: "#000",
+  textLight: "#FFF",
+  green: "#D8F162",
+  greenDark: "#D8FC39",
+  gray: "#8C8C8C",
+  bgBottomColor: "#181818",
+  bgColor: "#212121",
+};
---
+++ constants/METRICS.js
@@ -0,0 +1,4 @@
+export default METRIC = {
+  min: 2,
+  max: 4,
+};
